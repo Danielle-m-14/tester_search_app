2600ffead8fbc24ec50ab8e572adbe3eccc1378b
Danielle_M <daniellematalonis@gmail.com>
2022-04-17 20:16:42 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {ApolloClient, InMemoryCache, ApolloProvider,} from "@apollo/client";
+import './App.css';
+import Home from './Pages/Home.js';
+
+function App() {
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://graphql-weather-api.herokuapp.com/"
+  })
+
+  return (
+    <ApolloProvider client={client}>
+ 
+       <Home />
+  
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'; 
+import { useLazyQuery } from '@apollo/client';
+import { GET_WEATHER_QUERY } from '../graphql/Queries';
+
+function Home() {
+    const [citySearched, setCitySearched] = useState('');
+    const [getWeather, { loading, data, error }] = useLazyQuery(
+        GET_WEATHER_QUERY, 
+        {
+            variables: { name: citySearched },
+        }
+    );
+    if (loading) return <h1>Loading...</h1>
+    if (error) return <h1>Error found</h1>
+    if (data) {
+        console.log(data);
+    }
+
+    return (
+    <div className="home"> 
+    <h1>Search for Weather</h1>
+        <input type="text" placeholder="City name..." onChange={(event) => {
+            setCitySearched(event.target.value);
+        }
+        }  />
+        <button onClick={() => getWeather( {variables: {name: citySearched} })}> Search</button>
+        <div className="weather">
+            {data && (
+                <>
+                <h1> {data.getCityByName.name}</h1>
+                <h1> Temperature: {data.getCityByName.weather.temperature.actual}</h1>
+                <h1> Wind Speed: {data.getCityByName.weather.wind.speed}</h1>
+                <h1> Description: {data.getCityByName.weather.summary.description}</h1>
+                <h1> Icon: {data.getCityByName.weather.summary.icon}</h1>
+                </>
+            )}
+        </div>
+        </div>
+    );
+}
+
+export default Home; 
